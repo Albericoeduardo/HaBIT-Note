172f2d384199b778eac1b93c8f38856121ef7534
Albérico Eduardo <albericoeduardo202@gmail.com>
2024-11-30 16:59:25 -0300

    feat: add login page factory and sample view

---
+++ lib/Scenes/Login/login_factory.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:habit_note/Scenes/Login/login_view.dart';
+
+class LoginPageFactory {
+  static Widget create() {
+    return const LoginPage();
+  }
+}
