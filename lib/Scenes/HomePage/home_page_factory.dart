157ce686a4363dbb1c635b1a7475fcb8758f867a
Albérico Eduardo <albericoeduardo202@gmail.com>
2024-12-01 15:37:46 -0300

    refactor: update HomePageFactory to remove username parameter

--- lib/Scenes/HomePage/home_page_factory.dart
+++ lib/Scenes/HomePage/home_page_factory.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:habit_note/Scenes/HomePage/home_page_view.dart';
 
 class HomePageFactory {
-  static Widget create(username, emailAddress, password) {
+  static Widget create(emailAddress, password) {
     print("HomePageFactory" + emailAddress + password);
     return const HomePage();
   }
