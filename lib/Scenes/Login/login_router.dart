99a855a9bac492383a07da8769254b08974a943e
Albérico Eduardo <albericoeduardo202@gmail.com>
2024-12-01 15:36:42 -0300

    feat: add login page view and router

--- lib/Scenes/Login/login_router.dart
+++ lib/Scenes/Login/login_router.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:habit_note/Scenes/HomePage/home_page_factory.dart';
+import 'package:habit_note/Scenes/Registration/registration_factory.dart';
 
 class LoginPageRouter {
-  static void goToHomePage(BuildContext context, username, emailAddress, password) {
+  static void goToRegistrationPage(BuildContext context) {
     Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) => HomePageFactory.create(username, emailAddress, password))
+      MaterialPageRoute(builder: (context) => RegistrationPageFactory.create())
+    );
+  }
+
+  static void goToHomePage(BuildContext context, emailAddress, password) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => HomePageFactory.create(emailAddress, password))
     );
   }
 }
